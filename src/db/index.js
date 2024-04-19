8b7dd6f546ed35b056c388cf57f2924e6c471d75
Suhan376 <zendev192223@gmail.com>
2024-04-19 10:51:45 +0530

    SEVENTH COMMIT

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-import { DB_name } from "../constants";
+import { DB_name } from "../constants.js";
 
 
 const connectDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_name}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`);
         console.log(`Mongo DB connected !! DB host ${connectionInstance.connection.host}`);
-        console.log(connectionInstance)
+        // console.log(connectionInstance)
     } catch (error) {
 
         console.log("Error",error);
